b3886815d1b1acba3f5aaee95ae9aedbb9e2ecf7
Nikhil Vats <nikhilvats208@gmail.com>
2026-07-08 13:28:48 +0530

    Time: 2 ms (10.2%), Space: 30.6 MB (12.35%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -4,9 +4,7 @@ public:
         vector <int> temp(nums.size());
         for(int i=0; i< nums.size(); i++){
             temp[(i+k)%nums.size()]=nums[i];
-        }
-        for(int i=0; i<nums.size(); i++){
-            nums[i]=temp[i];
-        }
+        }       
+            nums = std::move(temp);       
     }
 };
