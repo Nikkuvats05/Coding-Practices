7b8eb7061df5dd9ffb03dfca1583927b2061fe8f
Nikhil Vats <nikhilvats208@gmail.com>
2026-07-08 14:19:55 +0530

    Time: 484 ms (6.04%), Space: 12.6 MB (64.83%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -6,15 +6,16 @@ public:
         }
         int count=0;
     vector<bool>prime(n,true); 
-    for(int i=2; i<n; i++){
-        if(prime[i]){
-            count++;
-        
-            for(long long j=(long long) i*i; j<n; j=j+i){
+    for(int i=2; i*i<n; i++){   
+            for(int j= i*i; j<n; j=j+i){
                 prime[j]=0;
             }
         }
+        for(int i=2; i<n; i++){
+    if(prime[i]){
+            count++;
     }
+        }
     return count;
     }
 };
