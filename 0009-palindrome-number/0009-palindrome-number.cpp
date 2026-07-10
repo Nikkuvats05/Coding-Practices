c4fa217a6b042532cb7f071ee0049cb279d25bf8
Nikhil Vats <nikhilvats208@gmail.com>
2026-07-10 23:46:49 +0530

    Time: 3 ms (44.87%), Space: 8.7 MB (38.09%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0){
-            return 0;
+       if (x < 0){
+            return false;
         }
-        vector <int>v;
-        while(x!=0){
-            int digit=x%10;
-             x=x/10;
-            v.push_back(digit);
+
+        long reverse = 0;
+        int xcopy = x;
+
+        while (x > 0) {
+            reverse = (reverse * 10) + (x % 10);
+            x /= 10;
+
+           // cout << reverse << " " << x << endl;
         }
-       int start=0;
-       int end=v.size()-1;
-       while(start<=end){
-        if(v[start]!=v[end]){
-            return 0;
-        }
-        start++;
-        end--;
-       }
-       return 1;
+
+        return reverse == xcopy; 
     }
 };
