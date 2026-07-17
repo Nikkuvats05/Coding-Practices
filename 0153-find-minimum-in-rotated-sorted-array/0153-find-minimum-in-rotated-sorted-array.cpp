63ada751f8acc8c89bc9141618cdc735526274b1
Nikhil Vats <nikhilvats208@gmail.com>
2026-07-17 23:59:53 +0530

    Time: 0 ms (100%), Space: 14.2 MB (11.88%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
+   
     int findMin(vector<int>& nums) {
-        if(nums.size()==1){
-            return nums[0];
-        }
-        int s=0;
-        int e=nums.size()-1;
-        int min=nums[0];
-        for(int i=1; i<nums.size(); i++){
-            if(nums[i]<min){
-                min=nums[i];
+        int s = 0;
+        int e = nums.size() - 1;
+        
+        while (s < e) {
+            int mid = s + (e - s) / 2;
+            
+            // Agar mid element last element se bada hai, 
+            // iska matlab minimum element right side mein hai
+            if (nums[mid] > nums[e]) {
+                s = mid + 1;
+            } 
+            // Agar chhota ya barabar hai, toh minimum left side mein hai (ya khud mid hai)
+            else {
+                e = mid;
             }
         }
-        return min;
+        
+        // Loop khatam hone par s aur e dono minimum element par rukenge
+        return nums[s];
     }
 };
