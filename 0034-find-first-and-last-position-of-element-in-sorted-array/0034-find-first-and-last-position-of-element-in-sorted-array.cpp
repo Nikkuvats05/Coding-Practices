aa9b183c6ffe6a3a1b62954f83351c4134abace8
Nikhil Vats <nikhilvats208@gmail.com>
2026-07-22 00:11:14 +0530

    Time: 0 ms (100%), Space: 17.5 MB (54.6%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,42 +1,56 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        #include <vector>
-        int first = findBound(nums, target, true);
-        
-        // Agar target array mein hai hi nahi, toh direct [-1, -1] return kar do
-        if (first == -1) {
-            return {-1, -1};
+       int s=0;
+       int e=nums.size()-1;
+       vector<int>ans;
+       int find1=0;
+       int find2=0;
+       int firstoccur;
+       while(s<=e){
+        int mid= s+(e-s)/2;
+
+        if(nums[mid]==target){
+        firstoccur=mid;
+        e=mid-1;
+        find1=1;
         }
-        
-        int last = findBound(nums, target, false);
-        return {first, last};
-    }
-
-private:
-    int findBound(const vector<int>& nums, int target, bool isFirst) {
-        int left = 0;
-        int right = nums.size() - 1;
-        int bound = -1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target) {
-                bound = mid; // Target mil gaya, potential index save kar lo
-                if (isFirst) {
-                    right = mid - 1; // Left side check karo first occurrence ke liye
-                } else {
-                    left = mid + 1;  // Right side check karo last occurrence ke liye
-                }
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+        else if(nums[mid]<target){
+            s=mid+1;
         }
+        else{
+            e=mid-1;
+        }
+       }
+       ans.push_back(firstoccur);
 
-        return bound;
+       int lastoccur;
+       s=0;
+       e=nums.size()-1;
+
+        while(s<=e){
+        int mid= s+(e-s)/2;
+
+        if(nums[mid]==target){
+        lastoccur=mid;
+        s=mid+1;
+        find2=1;
+        }
+        else if(nums[mid]<target){
+            s=mid+1;
+        }
+        else{
+            e=mid-1;
+        }
+       }
+       ans.push_back(lastoccur);
+       if(find1==1 && find2==1){
+        return ans;
+       }
+       vector<int>anss;
+       anss.push_back(-1);
+       anss.push_back(-1);
+       return anss;
     }
 };
  
