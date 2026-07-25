5523a05fa8e44719ea8aacca823a613d3ffa5e40
Nikhil Vats <nikhilvats208@gmail.com>
2026-07-26 00:26:36 +0530

    Time: 0 ms (100%), Space: 8.6 MB (71.1%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,7 +1,27 @@
 class Solution {
+    private:
+    double power(double x, long long n){
+        if(n==0){
+            return 1.0;
+        }
+        else if(n==1){
+            return x;
+        }
+        double ans= power(x, n/2);
+        if(n%2==0){
+            return ans*ans;
+        }
+        else{
+            return x*ans*ans;
+        }
+    }
 public:
     double myPow(double x, int n) {
-        double ans= pow(x,n);
-        return ans;
+        long long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        return  power( x,  N);
     }
 };
