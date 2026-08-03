c6d1b72aaf8fe63077507603ad28c663bb754088
Nikhil Vats <nikhilvats208@gmail.com>
2026-08-03 22:30:05 +0530

    Time: 14 ms (6.98%), Space: 12.9 MB (11.56%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,22 +4,20 @@ public:
         if(s.size()!=t.size()){
             return false;
         }
-        int count=0;
-        for(int i=0; i<s.size(); i++){
-            for(int j=0; j<t.size(); j++){
-                if(s[i]==t[j]){
-                    count++;
-                    t.erase(t.begin()+j);
-                    break;
-                }
-            }
-        }
-        if(count==s.size()){
-            return true;
-        }
-        else{
+       vector<int>s1(s.length());
+       vector<int>t1(s.length());
+       for(int i=0; i<s.length(); i++){
+        s1.push_back(s[i]-'a');
+        t1.push_back(t[i]-'a');
+       }
+       sort(s1.begin(), s1.end());
+       sort(t1.begin(), t1.end());
+       for(int i=0; i<s1.size(); i++){
+        if(s1[i]!=t1[i]){
             return false;
         }
+       }
+       return true;
         
     }
 };
