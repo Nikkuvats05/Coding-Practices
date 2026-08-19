11a5ac3b6d86f0c34438fe759859b187d8e23aaf
Nikhil Vats <nikhilvats208@gmail.com>
2026-08-19 23:16:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (53.76%) - LeetSync

---
+++ 742-to-lower-case/to-lower-case.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        string temp;
+        for(int i=0; i<s.length(); i++){
+            if(s[i]>='A' && s[i]<='Z'){
+                temp.push_back(s[i]-'A'+'a');
+            }
+            else{
+                temp.push_back(s[i]);
+            }
+        }
+        return temp;
+    }
+};
